96692cf5e5efbe721145e9c6a044a331a9e15710
Ochko <ochkoo@gmail.com>
2013-09-01 14:10:37 +0900

    usage files for api testing

--- tests/execute.sh
+++ tests/execute.sh
@@ -12,51 +12,51 @@ gcc -o  tle    tle.c     -Wall -ansi -pedantic
 gcc -o  wtle   wtle.c    -Wall -ansi -pedantic 
 
 echo "*** Testing Memory Limit ***"
-../safeexec --exec mle > /dev/null
+../safeexec --usage usage/memory-limit-exceeded.txt --exec mle > /dev/null
 echo "------------------------------"
 
 echo "*** Testing Output Limit ***"
-../safeexec --exec ole    > /dev/null
+../safeexec --usage usage/output-limit-exceeded.txt --exec ole    > /dev/null
 echo "------------------------------"
 
 echo "*** Testing Time Limit ***"
-../safeexec --exec tle    > /dev/null
+../safeexec --usage usage/timi-limit-exceeded.txt --exec tle    > /dev/null
 echo "------------------------------"
 
 echo "*** Testing Wall Time Limit ***"
-../safeexec --clock 3 --exec wtle   > /dev/null
+../safeexec --usage usage/wall-time-limit-exceeded.txt --clock 3 --exec wtle   > /dev/null
 echo "------------------------------"
 
 echo "*** Testing Fork ***"
-../safeexec --exec fork
+../safeexec --usage usage/can-not-fork.txt --exec fork
 echo "------------------------------"
 
 echo "*** Testing IO ***"
-echo "100 Coder" | ../safeexec --exec io
+echo "100 Coder" | ../safeexec --usage usage/read-stdinput.txt --exec io
 echo "------------------------------"
 
 echo "*** Testing std err output ***"
-../safeexec --exec err
-echo "  std err is redirected to /dev/null"
-../safeexec --error err.out --exec err
-echo "  std err is redirected to err.out"
+../safeexec --usage usage/stderr-to-devnull.txt --exec err
+echo "std err is redirected to /dev/null"
+../safeexec --usage usage/stderr-to-file.txt --error err.out --exec err
+echo "std err is redirected to err.out:"
 cat err.out
 rm -f err.out
 echo "------------------------------"
 
 echo "*** Testing file writing ***"
-../safeexec --exec write
+../safeexec --usage usage/file-write-attempt.txt --exec write
 echo "------------------------------"
 
 echo "*** Testing file reading ***"
-../safeexec --error err.out --exec read
+../safeexec --usage usage/file-read-attempt.txt --error err.out --exec read
 echo "------------------------------"
 cat err.out
 rm -f err.out
 echo "------------------------------"
 
 echo "*** Testing Exit code ***"
-../safeexec --exec return > /dev/null
+../safeexec --usage usage/nonzero-exit-code.txt --exec return > /dev/null
 echo "------------------------------"
 
 rm -f err read write fork io mle ole return tle wtle
