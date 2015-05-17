f7c21d909432553572ae43bba3e90a465fd37aad
Ochko <ochkoo@gmail.com>
2015-05-17 21:04:50 +0900

    fix file tests

--- tests/test-read.c
+++ tests/test-read.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main(void)
+
+int main(int argc, char *argv[])
 {
   FILE *fp;
   char ch;
- 
-  fp = fopen("secret.txt","r");
+
+  fp = fopen("tests/secret.txt" ,"r");
   if( fp == NULL ) {
     perror("Error while opening the file");
     exit(EXIT_FAILURE);
   }
- 
+
   printf("Oops! This file ");
- 
+
   while( ( ch = fgetc(fp) ) != EOF )
     printf("%c",ch);
 
